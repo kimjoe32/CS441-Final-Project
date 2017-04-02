9646b257ed12ee00e207affb43bd79e432a23e12
ETS Admin <etsadmin@ETSMacbook1.local>
2017-04-02 15:31:18 -0400

    changed project type

---
+++ Final Project-Kim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Final Project-Kim
+//
+//  Created by ETS Admin on 4/2/17.
+//  Copyright © 2017 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Final Project-Kim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Final Project-Kim
+//
+//  Created by ETS Admin on 4/2/17.
+//  Copyright © 2017 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
